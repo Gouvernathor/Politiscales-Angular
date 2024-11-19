98d0fc272d3243447205aa815defca7c9ca7da51
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-11-19 18:30:47 +0100

    Re-export languageIds

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,6 +3,7 @@ import rawQuestions from '../assets/questions.json';
 import { Question } from './datamodel/questionsConfiguration';
 import { getQuestions } from './services/configurationService';
 
+export {languageIds};
 const translations: Readonly<Record<string, Readonly<Record<string, string>>>> = Object.fromEntries(await Promise.all(
     languageIds.map(async (languageId) => (
         [languageId, await import(`../assets/languages/${languageId}.json`)]

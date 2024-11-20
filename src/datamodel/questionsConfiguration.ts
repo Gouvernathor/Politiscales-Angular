4afbecdb6b69e617415599c3e08941f1623960d3
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-11-21 00:37:09 +0100

    Move question ids around

--- src/datamodel/questionsConfiguration.ts
+++ src/datamodel/questionsConfiguration.ts
@@ -16,8 +16,6 @@ export enum Axis {
     Productivism = -Ecology,
     Reformism = -Revolution,
 }
-// for (let axisname in Axis)
-//   const axisvalue = Axis[axisname]
 
 export enum SpecialAxis {
     Feminism = 10,
@@ -28,7 +26,6 @@ export enum SpecialAxis {
     Veganism,
     Anarchism,
 }
-// compute the max value of each axis dynamically when loading configurations
 
 export interface AnswerValue {
     readonly axis: Axis|SpecialAxis;
@@ -39,4 +36,42 @@ export interface Question {
     readonly id: string;
     readonly valuesYes: AnswerValue[];
     readonly valuesNo: AnswerValue[];
+}
+
+const axisById = new Map<string, Axis|SpecialAxis>([
+    ["c0", Axis.Constructivism],
+    ["c1", Axis.Essentialism],
+    ["b0", Axis.Internationalism],
+    ["b1", Axis.Nationalism],
+    ["p0", Axis.Communism],
+    ["p1", Axis.Capitalism],
+    ["m0", Axis.Regulationnism],
+    ["m1", Axis.LaissezFaire],
+    ["s0", Axis.Progressivism],
+    ["s1", Axis.Conservatism],
+    ["j0", Axis.JusticeSoft],
+    ["j1", Axis.JusticeHard],
+    ["e0", Axis.Ecology],
+    ["e1", Axis.Productivism],
+    ["t0", Axis.Revolution],
+    ["t1", Axis.Reformism],
+
+    ["femi", SpecialAxis.Feminism],
+    ["anar", SpecialAxis.Anarchism],
+    ["comp", SpecialAxis.Conspiracy],
+    ["mona", SpecialAxis.Monarchism],
+    ["prag", SpecialAxis.Pragmatism],
+    ["reli", SpecialAxis.Religion],
+    ["vega", SpecialAxis.Veganism],
+]);
+export function getAxisId(axis: Axis|SpecialAxis): string|null {
+    for (const [id, value] of axisById.entries()) {
+        if (value === axis) {
+            return id;
+        }
+    }
+    return null;
+}
+export function getAxisFromId(id: string): Axis|SpecialAxis|null {
+    return axisById.get(id) || null;
 }

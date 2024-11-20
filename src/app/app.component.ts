53368107a0df0fdc5e66cdcf49aea80d645aba8e
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-11-20 01:00:47 +0100

    Add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { getLine } from '../unsorted/configuration';
-import { LanguageSelectorComponent } from './language-selector/language-selector.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LanguageSelectorComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'politiscales';
   localize = getLine;
-  pageLayout?: string;
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { LanguageSelectorComponent } from './language-selector/language-selector.component';
+import { languageIds } from '../unsorted/configuration';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // localized paths, each with children: empty, help, quiz, results
+    // to related component with parameter set to the language id
+    ...languageIds.map((languageId) => ({
+        path: languageId,
+        data: {lang: languageId}, // TODO: check that it works
+        children: [
+            // {
+            //     path: 'help',
+            //     component: HelpComponent,
+            // },
+            // {
+            //     path: 'quiz',
+            //     component: QuizComponent,
+            // },
+            // {
+            //     path: 'results',
+            //     component: ResultsComponent,
+            // },
+            // {
+            //     path: '',
+            //     component: StartComponent,
+            // },
+        ]
+    })),
+
+    // help (will improvize language based on navigator language)
+    // optional, results/quiz (will improvize language too)
+
+    // empty for language selector
+    {
+        path: '',
+        component: LanguageSelectorComponent,
+    },
+
+    // optional, wildcard redirect to empty
+];

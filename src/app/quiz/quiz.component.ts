35ea3b33668e1ac4475a679905b4d0ce013440dc
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-11-20 20:45:26 +0100

    Quiz implementation skeleton

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { getLine, setLanguage } from '../../unsorted/configuration';
+import { getLine, questions as baseQuestions, setLanguage } from '../../unsorted/configuration';
 import { firstValueFrom } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { Question } from '../../datamodel/questionsConfiguration';
 
 @Component({
   selector: 'app-quiz',
@@ -12,7 +13,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class QuizComponent {
   localize = getLine;
-  constructor(private route: ActivatedRoute) {}
+  private question_number: number; // TODO make a property, computed from answers ?
+  private readonly questions: Question[];
+  constructor(private route: ActivatedRoute) {
+    this.question_number = 0;
+
+    let questions = baseQuestions.slice();
+    for (let j, i = questions.length; i>0; i--) {
+      j = Math.floor(Math.random()*(i+1));
+      [questions[i], questions[j]] = [questions[j], questions[i]];
+    }
+    this.questions = questions;
+  }
 
   async ngOnInit() {
     let lang: string;
@@ -20,5 +32,27 @@ export class QuizComponent {
     if (lang) {
       setLanguage(lang);
     }
+
+    this.initQuestion();
+  }
+
+  private initQuestion() {
+    // TODO
+  }
+
+  answer(answer: any) {
+    // TODO (previously next_question)
+  }
+
+  prevQuestion() {
+    // TODO
+  }
+
+  gotoResults() {
+    // TODO (previously results)
+  }
+
+  simulate() {
+    // TODO
   }
 }

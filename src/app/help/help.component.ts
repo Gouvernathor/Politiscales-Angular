f48e760e61706d49fede1992c56b4367530ceca4
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-11-21 21:00:59 +0100

    Fill the Help page

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+import { getLine, setLanguage } from '../../unsorted/configuration';
 
 @Component({
   selector: 'app-help',
@@ -8,5 +11,14 @@ import { Component } from '@angular/core';
   styleUrl: './help.component.css'
 })
 export class HelpComponent {
+  localize = getLine;
+  constructor(private route: ActivatedRoute) {}
 
+  async ngOnInit() {
+    let lang: string;
+    lang = (await firstValueFrom(this.route.data))["lang"];
+    if (lang) {
+      setLanguage(lang);
+    }
+  }
 }

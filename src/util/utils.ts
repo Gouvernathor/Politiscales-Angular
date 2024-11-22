04a1d8e5463ce5074e0d7fa64b68d193bd5b8e50
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-11-22 22:50:21 +0100

    Check in a deterministic RNG

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -23,3 +23,55 @@ export function sorted<T>(array: Iterable<T>, key?: ((_:T) => any), reverse: boo
         return rv;
     });
 }
+
+/**
+ * Deterministic and reseedable RNG with utility methods
+ */
+export class RNG {
+    private readonly m;
+    private readonly a;
+    private readonly c;
+    private state!: number;
+    /**
+     * @param seed integer between 0 and 2^31 - 1 ; if not passed, generates it from Math.random()
+     */
+    constructor(seed?: number) {
+        // LCG using GCC's constants
+        this.m = 0x80000000; // 2**31;
+        this.a = 1103515245;
+        this.c = 12345;
+
+        this.seed = seed;
+    }
+    /**
+     * @param seed integer between 0 and 2^31 - 1 ; if undefined, generates it from Math.random()
+     */
+    set seed(seed: number|undefined) {
+        this.state = seed ? seed : Math.floor(Math.random() * (this.m - 1));
+    }
+    /**
+     * @returns a number presumably in [[0, 2**31[[
+     */
+    randInt() {
+        this.state = (this.a * this.state + this.c) % this.m;
+        return this.state;
+    }
+    /**
+     * @returns a number in [0, 1[
+     */
+    random() {
+        return this.randInt() / this.m;
+    }
+    /**
+     * @returns a number in [[min, max[[
+     */
+    randRange(min: number, max: number) {
+        return min + Math.floor(this.random() * (max - min));
+    }
+    /**
+     * @returns one of the elements
+     */
+    choice<T>(array: string | T[]) {
+        return array[this.randRange(0, array.length)];
+    }
+}

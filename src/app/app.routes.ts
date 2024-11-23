edf9b5d14231fc8bf9ef05ef305cdb09b5d86152
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2024-11-23 01:53:06 +0100

    Whitespace

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,37 +11,19 @@ export const routes: Routes = [
     // to related component with parameter set to the language id
     ...languageIds.map((languageId) => ({
         path: languageId,
-        data: {lang: languageId},
+        data: { lang: languageId },
         children: [
-            {
-                path: 'help',
-                component: HelpComponent,
-            },
-            {
-                path: 'quiz',
-                component: QuizComponent,
-            },
-            {
-                path: 'results',
-                component: ResultsComponent,
-            },
-            {
-                path: '',
-                component: StartComponent,
-            },
-        ]
+            { path: 'help', component: HelpComponent, },
+            { path: 'quiz', component: QuizComponent, },
+            { path: 'results', component: ResultsComponent, },
+            { path: '', component: StartComponent, },
+        ],
     })),
 
     // help (will improvize language based on navigator language)
-    {
-        path: 'help',
-        component: HelpComponent,
-    },
+    { path: 'help', component: HelpComponent, },
     // optional, results/quiz (will improvize language too)
 
     // empty (or optionally wildcard) for language selector
-    {
-        path: '',
-        component: LanguageSelectorComponent,
-    },
+    { path: '', component: LanguageSelectorComponent, },
 ];

c3a5acc3af3412f5d263cd11b3acb4956725c0ad
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-29 16:17:48 +0100

    convert language-selector

--- src/app/language-selector/language-selector.component.ts
+++ src/app/language-selector/language-selector.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { ClickCursorDirective } from '../style.directive';
+import { RouterLink } from '@angular/router';
 import { getLine, languageIds } from '../../services/localizationService';
 
 @Component({
   selector: 'app-language-selector',
-  imports: [ClickCursorDirective],
+  imports: [RouterLink],
   templateUrl: './language-selector.component.html',
   styleUrl: './language-selector.component.css'
 })
 export class LanguageSelectorComponent {
   localize = getLine;
   languageIds = languageIds;
-
-  constructor(private router: Router) {}
-
-  onLanguageChange(lang: string) {
-    this.router.navigate([lang]);
-  }
 }
